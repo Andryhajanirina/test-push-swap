1f76ef9893eabdb38e02a13e53ea090d031171ef
Andry Hajanirina Andrianjatoharinavalona <andry-ha@c1r9s3.42antananarivo.mg>
2026-03-11 15:41:04 +0300

    Add features/parsing

---
+++ parsing/check_duplicates.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   check_duplicates.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andry-ha <andry-ha@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/08 15:27:52 by andry-ha          #+#    #+#             */
+/*   Updated: 2026/03/09 11:37:24 by andry-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+
+int	check_duplicates(t_stack *stack)
+{
+	t_stack	*current;
+	t_stack	*runner;
+
+	current = stack;
+	while (current)
+	{
+		runner = current->next;
+		while (runner)
+		{
+			if (current->value == runner->value)
+				return (1);
+			runner = runner->next;
+		}
+		current = current->next;
+	}
+	return (0);
+}
---
+++ parsing/parse_args.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_args.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: andry-ha <andry-ha@student.42antananari    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/08 14:50:30 by andry-ha          #+#    #+#             */
+/*   Updated: 2026/03/09 11:37:24 by andry-ha         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../push_swap.h"
+#include "./libft/libft.h"
+
+int	parse_args(int argc, char **argv, t_stack **a)
+{
+	int	i;
+
+	i = 1;
+	while (i < argc)
+	{
+		add_back(a, ft_atoi(argv[i]));
+		i++;
+	}
+	if (check_duplicates(a))
+		return (0);
+	return (1);
+}
